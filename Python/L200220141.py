6695185ee29b969d02dd1d2366e349a65802aeb0
Fnrzz <faridnurrganteng123@gmail.com>
2022-12-20 10:00:34 +0700

    [push] file python farid

--- Python/L200220141.py
+++ Python/L200220141.py
@@ -1,1 +1,15 @@
-print("Farid Nur Raidananda")
+class Prisma(object):
+    def __init__(self, panjang, lebar, tinggi):
+       self.p = panjang
+       self.t = tinggi
+       self.l = lebar
+
+    def luas(self): #ini adalah method
+        """ Mengembalikan luas prisma segiempat ini """
+        luas_alas = self.p * self.l
+        keliling = 2 * (self.p + self.l)
+        luas_total =(2*luas_alas)  + (keliling*self.t)
+        return luas_total
+
+bangun = Prisma(12, 6,10)
+print (bangun.luas())
