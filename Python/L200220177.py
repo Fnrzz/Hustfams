51a941c6d0f0ab8fe17e7239779ad8f2f9397c44
aljawary <aljaprant12@gmail.com>
2022-12-20 09:44:11 +0700

    jajargenjang default value

--- Python/L200220177.py
+++ Python/L200220177.py
@@ -1,1 +1,16 @@
-print("test")
+print("jajargenjang")
+
+class jajargenjang:
+    
+    #default value for a is 5 & t is 10
+    a = 5
+    t = 10
+    
+    def hitung(self):
+        calculate = self.a * self.t
+        return calculate
+    
+
+p = jajargenjang()
+print("alas: ", p.a)
+print("luas: ", p.hitung())
