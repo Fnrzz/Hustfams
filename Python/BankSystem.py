e296018bcfb9a6e89eefa8b80e42e1f1a9373ece
aljawary <aljaprant12@gmail.com>
2023-01-18 00:18:50 +0700

    [Feat] Bank System Example

---
+++ Python/BankSystem.py
@@ -0,0 +1,39 @@
+
+class bankAccount:
+    def __init__(self, saldo, tipe_akun):
+        self.saldo = saldo
+        self.tipe_akun = tipe_akun
+        
+    def depo(self, amount = 0):
+        pilih = int(input("masukkan nominal depo: "))
+        pilih + amount
+        self.saldo += pilih
+        return self.saldo
+        
+    def wd(self, amount = 0):
+        pilih = int(input("masukkan nominal wd: "))
+        pilih + amount
+        if pilih > self.saldo:
+            print('saldo kurang')
+        else:
+            self.saldo -= pilih
+            return self.saldo
+        
+    def cekSaldo(self):
+        return self.saldo
+    
+#object 
+my = bankAccount(0, 'reguler')
+
+while True:
+    choice = input("masukkan pilihan: ")
+    if choice == '1':
+        print(f'saldo anda adalah ${my.cekSaldo()}')
+    elif choice == '2':
+        my.depo()
+    elif choice == '3':
+        my.wd()
+    else:
+        print("perintah yang anda masukkan invalid!")
+        break
+    
